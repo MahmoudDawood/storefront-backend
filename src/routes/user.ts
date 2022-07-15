29738a099705b1f557c568e9851072870fb30627
MahmoudDawood <mm.dawood16@yahoo.com>
2022-07-15 10:43:47 +0200

    add user authentication and authorization middlewares

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,23 +1,62 @@
 import { UserStore } from '../Models/user';
-import { Router, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { Router, Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const userRouter = Router();
 const store = new UserStore();
 
-const index = async (_req: Request, res: Response) => {
+function verifyAuthToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    const token = authorizationHeader?.split(' ')[1];
+    jwt.verify(token as string, process.env.TOKEN_SECRET as string);
+    next();
+  } catch (err) {
+    res.status(401);
+    res.json('Access denied, Invalid token: ' + err);
+  }
+}
+
+function verifyAuthorization(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    const token = authorizationHeader?.split(' ')[1];
+    const payload = jwt.verify(
+      token as string,
+      process.env.TOKEN_SECRET as string
+    ) as JwtPayload;
+    const userId = payload.user.id;
+    if (userId !== +req.params.id) {
+      throw new Error('User id does not match!');
+    }
+    next();
+  } catch (err) {
+    res.status(401);
+    res.json('Access denied, Invalid token: ' + err);
+  }
+}
+
+const index = async (_req: Request, res: Response): Promise<void> => {
   const result = await store.index();
   res.send(result);
 };
 
-const show = async (_req: Request, res: Response) => {
+const show = async (_req: Request, res: Response): Promise<void> => {
   const result = await store.show(parseInt(_req.params.id));
   res.send(result);
 };
 
-const create = async (_req: Request, res: Response) => {
+const create = async (_req: Request, res: Response): Promise<void> => {
   const user = {
     name: _req.body.name,
     phone: _req.body.phone,
@@ -31,39 +70,59 @@ const create = async (_req: Request, res: Response) => {
       { user: result },
       process.env.TOKEN_SECRET as string
     );
-    // console.log(token);
     res.json(token);
   } catch (err) {
-    res.status(401);
-    res.send(err);
+    res.status(400);
+    res.json(err);
   }
 };
 
-const authenticate = async (_req: Request, res: Response) => {
+const authenticate = async (_req: Request, res: Response): Promise<void> => {
   const username = _req.body.username;
   const password = _req.body.password;
-  const result = await store.authenticate(username, password);
-  result === null ? res.status(400) : res.status(200);
-  res.send(result);
+  try {
+    const result = await store.authenticate(username, password);
+    const token = jwt.sign(
+      { user: result },
+      process.env.TOKEN_SECRET as string
+    );
+    if (result === null) res.status(400).json('User not found.');
+    else res.status(200).json(token);
+  } catch (err) {
+    res.status(401).json(`User Authentication Failed: ${err}`);
+  }
 };
 
-const deleteUser = async (_req: Request, res: Response) => {
+const deleteUser = async (req: Request, res: Response): Promise<void> => {
   try {
-    const token = _req.headers.authorization?.split(' ')[1];
-    if (jwt.verify(token as string, process.env.TOKEN_SECRET as string)) {
-      const result = await store.delete(parseInt(_req.params.id));
-      if (result) res.json(result);
-      /////////////// FINISH CHECKBOX 3, FIXING STATUS CODES & ERROR HANDLING ///////////////
-    }
+    const result = await store.delete(parseInt(req.params.id));
+    res.json(result);
   } catch (err) {
     res.status(400).json(err);
   }
 };
 
+const test = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    const token = authorizationHeader?.split(' ')[1];
+    const payload = jwt.verify(
+      token as string,
+      process.env.TOKEN_SECRET as string
+    ) as JwtPayload;
+    console.log(payload.user.id);
+    res.json(payload);
+  } catch (err) {
+    res.status(401);
+    res.json('Invalid token: ' + err);
+  }
+}; // Only for testing endpoint functionalities
+
 userRouter.get('/', index);
+userRouter.get('/test', test);
 userRouter.get('/:id', show);
 userRouter.post('/', create);
 userRouter.post('/authenticate', authenticate);
-userRouter.delete('/:id', deleteUser);
+userRouter.delete('/:id', verifyAuthToken, verifyAuthorization, deleteUser);
 
 export default userRouter;
