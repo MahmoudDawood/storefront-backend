1abb4bfb532727c69a43e99049af900f54b78d71
MahmoudDawood <mm.dawood16@yahoo.com>
2022-07-21 14:42:40 +0200

    update git config ignorecase to false

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  try {
+    const authorizationHeader = req.headers.authorization;
+    const token = authorizationHeader?.split(' ')[1];
+    jwt.verify(token as string, process.env.TOKEN_SECRET as string);
+    next();
+  } catch (err) {
+    res.status(401);
+    res.json('Access denied, Invalid token: ' + err);
+  }
+}
